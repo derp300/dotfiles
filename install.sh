d6dbb1a5626687ad03e099086c3fd96df980289d
Leonid Vinogradov <l.vinogradov@hh.ru>
2018-08-28 10:21:17 +0300

    add terminator

--- install.sh
+++ install.sh
@@ -3,10 +3,14 @@ rm -rf ~/.vimrc
 rm -rf ~/.vim
 git submodule init
 git submodule update
-ln -s $PWD/.vimrc ~/.vimrc
-ln -s $PWD/.vim ~/.vim
+ln -s $PWD/vim/.vimrc ~/.vimrc
+ln -s $PWD/vim/.vim ~/.vim
 
 #-----------------------ya-disk-----------
 rm -rf ~/.config/yandex-disk/config.cfg
-ln -s $PWD/config.cfg ~/.config/yandex-disk/config.cfg
+ln -s $PWD/yadisk/config.cfg ~/.config/yandex-disk/config.cfg
+
+#----------------------terminator---------
+rm -rf ~/.config/terminator/config
+ln -s $PWD/terminator/config ~/.config/terminator/config
 
