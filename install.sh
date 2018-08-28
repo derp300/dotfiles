60d0d873955c9867bc49d0f81b159e6cf4d60485
Leonid Vinogradov <leonid.52669@gmail.com>
2018-08-28 08:48:04 +0300

    ya-disk config

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
+#------------------------vim--------------
 rm -rf ~/.vimrc
 rm -rf ~/.vim
 git submodule init
 git submodule update
 ln -s $PWD/.vimrc ~/.vimrc
 ln -s $PWD/.vim ~/.vim
+
+#-----------------------ya-disk-----------
+rm -rf ~/.config/yandex-disk/config.cfg
+ln -s $PWD/config.cfg ~/.config/yandex-disk/config.cfg
 
