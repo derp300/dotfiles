99a95bb7124ade57b2072993a89cac2d431d9375
Leonid Vinogradov <leonid.52669@gmail.com>
2018-10-08 19:41:00 +0000

    remove yadisk

--- install.sh
+++ install.sh
@@ -6,10 +6,6 @@ git submodule update
 ln -s $PWD/vim/.vimrc ~/.vimrc
 ln -s $PWD/vim/.vim ~/.vim
 
-#-----------------------ya-disk-----------
-rm -rf ~/.config/yandex-disk/config.cfg
-ln -s $PWD/yadisk/config.cfg ~/.config/yandex-disk/config.cfg
-
 #----------------------terminator---------
 rm -rf ~/.config/terminator/config
 ln -s $PWD/terminator/config ~/.config/terminator/config
