62c654ca2ced4e622ac909fbe9cb5090e45d940f
Leonid Vinogradov <leonid.52669@gmail.com>
2018-10-13 10:07:23 +0300

    qqq

--- install.sh
+++ install.sh
@@ -10,27 +10,6 @@ ln -s $PWD/vim/.vim ~/.vim
 rm -rf ~/.config/terminator/config
 ln -s $PWD/terminator/config ~/.config/terminator/config
 
-#------------------------i3---------------
-rm -rf ~/.config/i3/config
-ln -s $PWD/i3/config ~/.config/i3/config
-
-#------------------------xinit------------
-rm -rf ~/.xinitrc
-ln -s $PWD/xinit/.xinitrc ~/.xinitrc
-
-#-------------------------bash------------
-rm -rf ~/.bash_profile
-ln -s $PWD/bash/.bash_profile ~/.bash_profile
-
-#-------------------------twmn------------
-rm -rf ~/.config/twmn/twmn.conf
-ln -s $PWD/twmn/twmn.conf ~/.config/twmn/twmn.conf
-
-#-------------------------gxkb------------
-rm -rf ~/.config/gxkb/gxkb.cfg
-ln -s $PWD/gxkb/gxkb.cfg ~/.config/gxkb/gxkb.cfg
-
-
 
 
 
