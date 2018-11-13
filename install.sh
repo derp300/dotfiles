ef85d5087c248737653af50d6f4e4cae020c2647
Leonid Vinogradov <leonid.52669@gmail.com>
2018-11-13 16:28:40 +0300

    qqq

--- install.sh
+++ install.sh
@@ -1,14 +1,7 @@
-#------------------------vim--------------
-rm -rf ~/.vimrc
-rm -rf ~/.vim
-git submodule init
-git submodule update
-ln -s $PWD/vim/.vimrc ~/.vimrc
-ln -s $PWD/vim/.vim ~/.vim
 
 #----------------------terminator---------
-#rm -rf ~/.config/terminator/config
-#ln -s $PWD/terminator/config ~/.config/terminator/config
+rm -rf ~/.config/terminator/config
+ln -s $PWD/public/terminator/config ~/.config/terminator/config
 
 
 
